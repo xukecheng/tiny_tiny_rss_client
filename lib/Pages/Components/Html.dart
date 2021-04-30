44b4aa599a8fa89ebe4ada353c7fe2d85d3ee714
许珂诚 <cnxukecheng@gmail.com>
2021-04-30 20:52:47 +0800

    老代码提交备份

--- lib/Pages/Components/Html.dart
+++ lib/Pages/Components/Html.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:html/dom.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:html_unescape/html_unescape.dart';
-// import '../../test1.dart';
+import '../../test.dart';
 
 var unescape = new HtmlUnescape();
 
@@ -12,19 +12,19 @@ class Html {
   htmlParser(html) {
     List<Flutter.Widget> parserdList = new List();
     List nodeList = new List();
-    Document document = parse(html);
+    Document document = parse(test);
 
     findBody(value) {
       value.forEach((element) {
         if (element.toString() == '<html body>') {
-          return element.nodes;
+          nodeList = element.nodes;
         } else {
           findBody(element.nodes);
         }
       });
     }
-    
-    nodeList = findBody(document.nodes[0].nodes);
+
+    findBody(document.nodes[0].nodes);
     nodeList.forEach((value) {
       // 查看每一个 Node 标签，是否为标题类型
       if (value.toString() == '<html h2>') {
@@ -48,62 +48,73 @@ class Html {
   nodesSplit(List nodes) {
     List tempList = new List();
 
-    for (var i = 0; i < nodes.length; i++) {
-      // nodeType = 3 = Node.TEXT_NODE (Element 或者 Attr 中实际的 文字)
-      if (nodes[i].nodeType == 3) {
-        tempList.add(Flutter.TextSpan(text: unescape.convert(nodes[i].text)));
-      } else if (nodes[i].localName == 'img') {
-        tempList.add(CachedNetworkImage(
-          imageUrl: nodes[i].attributes['src'],
-          placeholder: (context, url) => CircularProgressIndicator(),
-          errorWidget: (context, url, error) => Icon(Icons.error),
-        ));
-      } else if (nodes[i].localName == 'li') {
-        tempList.add(Flutter.Row(
-          children: [
-            Flutter.Text('\u{2022} '),
-            Flutter.RichText(
+    fact(nodes) {
+      for (var i = 0; i < nodes.length; i++) {
+        // nodeType = 3 = Node.TEXT_NODE (Element 或者 Attr 中实际的 文字)
+        if (nodes[i].nodeType == 3) {
+          tempList.add(Flutter.RichText(
               text: Flutter.TextSpan(
-                text: unescape.convert(nodes[i].text),
-                style: TextStyle(fontSize: 16.0, color: Colors.black),
-              ),
-            )
-          ],
-        ));
-      } else if (nodes[i].localName == 'a') {
-        tempList.add(Flutter.TextSpan(
-          text: unescape.convert(nodes[i].text),
-          style: TextStyle(fontSize: 16.0, color: Colors.blue),
-          // recognizer: 点击事件
-        ));
-        print(tempList);
-      } else if (nodes[i].localName == 'br') {
-        print("跳过 br");
-      } else {
-        tempList.add(nodesSplit(nodes[i].nodes));
-        print(tempList);
+                  style: TextStyle(fontSize: 16.0, color: Colors.black),
+                  text: unescape.convert(nodes[i].text))));
+        } else if (nodes[i].localName == 'img') {
+          tempList.add(CachedNetworkImage(
+            imageUrl: nodes[i].attributes['src'],
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ));
+        } else if (nodes[i].localName == 'li') {
+          tempList.add(Flutter.Row(
+            children: [
+              Flutter.Text('\u{2022} '),
+              Flutter.RichText(
+                text: Flutter.TextSpan(
+                  text: unescape.convert(nodes[i].text),
+                  style: TextStyle(fontSize: 16.0, color: Colors.black),
+                ),
+              )
+            ],
+          ));
+        } else if (nodes[i].localName == 'a') {
+          tempList.add(Flutter.RichText(
+              text: Flutter.TextSpan(
+            text: unescape.convert(nodes[i].text),
+            style: TextStyle(fontSize: 16.0, color: Colors.blue),
+            // recognizer: 点击事件
+          )));
+        } else if (nodes[i].localName == 'br') {
+          print("跳过 br");
+        } else {
+          fact(nodes[i].nodes);
+        }
       }
     }
 
+    fact(nodes);
+    tempList.removeWhere((value) => value == null);
     // 尝试将 Widget 从 list 中取出，然后在主 list 中添加 Widget 而非 List。
     try {
-      if (tempList[0] is TextSpan) {
-        List<Flutter.InlineSpan> spanList = new List();
-        tempList.forEach((e) => spanList.add(e));
-        return Flutter.RichText(
-          text: TextSpan(
-              style: TextStyle(fontSize: 16.0, color: Colors.black),
-              children: spanList),
-        );
-      } else if (tempList[0] != null) {
-        List<Widget> widgetList = new List();
-        // 有些中间插入的 Null 值需要排除
-        tempList.removeWhere((value) => value == null);
-        tempList.forEach((element) => widgetList.add(element));
-        return Flutter.Column(
-          children: widgetList,
-        );
-      }
+      // if (tempList[0] is TextSpan) {
+      //   List<Flutter.InlineSpan> spanList = new List();
+      //   tempList.forEach((e) => spanList.add(e));
+      //   return Flutter.RichText(
+      //     text: TextSpan(
+      //         style: TextStyle(fontSize: 16.0, color: Colors.black),
+      //         children: spanList),
+      //   );
+      // } else if (tempList[0] != null) {
+      //   List<Widget> widgetList = new List();
+      //   tempList.forEach((element) => widgetList.add(element));
+      //   return Flutter.Column(
+      //     children: widgetList,
+      //   );
+      // }
+      List<Widget> widgetList = new List();
+      print(tempList);
+      tempList.forEach((element) => widgetList.add(element));
+      print(widgetList);
+      return Flutter.Column(
+        children: widgetList,
+      );
     } catch (e) {}
   }
 }
